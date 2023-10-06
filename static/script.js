d9665d23d70dc31b394ef6bef4615fc8d2b9d8d4
hmuri <113418916+hmuri@users.noreply.github.com>
2023-10-06 22:57:03 +0900

    style: 반응형 css 추가

--- static/script.js
+++ static/script.js
@@ -7,21 +7,33 @@ function changeTheme() {
   nav.style.color = nav.style.color === "#333" ? "white" : "#333";
 }
 
-let postData;
-window.onload = function () {
-  fetch("/run-script")
+function changeColor(selectedItem) {
+  // 모든 항목에서 "selected" 클래스 제거
+  const items = document.querySelectorAll(".select-order");
+  items.forEach((item) => item.classList.remove("selected"));
+
+  // 선택된 항목에 "selected" 클래스 추가
+  selectedItem.classList.add("selected");
+}
+
+function fetchData(endpoint) {
+  fetch(endpoint)
     .then((response) => response.json())
     .then((data) => {
       if (data.success) {
-        postData = data.data;
-        console.log(postData);
+        const postData = data.data;
+        displayPosts(postData);
+      }
+    });
+}
 
-        const container = document.querySelector(".posting-container"); // 이 위치에 포스트 카드들을 추가합니다. 와우, posting-card는 클래스라서 앞에 점을 붙여줘야 한대요
-        //궁금증 : 이게 서버사이드 렌더링(서버에서 html 만들어서 클라이언트에 전달하는 개념)이라서 이렇게 html로 map 돌려서 html 텍스트 자체를 다 만들어놓고 한번에 렌더링 시키는 게 더 나은 게 맞는 건가? 보통 리액트 사용할 때는 클라이언트 측에서 맵 돌리면서 한 컴포넌트 씩 화면에 만들어내는 거고...? 그래서 csr이 로딩 걸리면서 하나씩 천천히 생겨나고, ssr은 한 번에 빵 하고 나타나고...?
-        postData.forEach((post) => {
-          const cardHTML = `
-          <div class="posting-card">
+function displayPosts(postData) {
+  const container = document.querySelector(".posting-container");
+  container.innerHTML = ""; // 기존 포스트 데이터를 지워줍니다.
 
+  postData.forEach((post) => {
+    const cardHTML = `
+            <div class="posting-card">
                 <a href="https://velog.io${post["post_url"]}" class="post-url">
                     <img src="${post["thumbnail_url"]}" alt="썸네일" class="thumbnail-url" />
                 </a>
@@ -39,11 +51,14 @@ window.onload = function () {
                     <b class="author-name">${post["author_name"]}</b>
                 </a>
                 <div class="likes">${post["likes"]} 좋아요</div>
-              </div>
+            </div>
         `;
 
-          container.insertAdjacentHTML("beforeend", cardHTML);
-        });
-      }
-    });
+    container.insertAdjacentHTML("beforeend", cardHTML);
+  });
+}
+
+// 페이지 로드 시 트렌딩 데이터 기본값
+window.onload = function () {
+  fetchData("/run-script");
 };
